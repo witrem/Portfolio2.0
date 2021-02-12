5370f565ec693066c9130c00fed3e3dc0eb85928
Alexis <alexislegeay@hotmail.fr>
2021-02-12 17:16:04 +0100

    edit navbar icon color

--- resources/views/partials/dark-mode-toggle.blade.php
+++ resources/views/partials/dark-mode-toggle.blade.php
@@ -2,8 +2,8 @@
     {{ csrf_field() }}
     {{ method_field('patch') }}
     @if(setting()->getForCurrentUser('dark-mode-enabled'))
-        <button class="config_view {{ $classes ?? '' }}"><span>@icon('light-mode')</span><span>{{ trans('common.light_mode') }}</span></button>
+        <button class="config_view {{ $classes ?? '' }}"><span class="icon-mobile">@icon('light-mode')</span><span class="icon-mobile">{{ trans('common.light_mode') }}</span></button>
     @else
-        <button class="config_view {{ $classes ?? '' }}"><span>@icon('dark-mode')</span><span>{{ trans('common.dark_mode') }}</span></button>
+        <button class="config_view {{ $classes ?? '' }}"><span class="icon-mobile">@icon('dark-mode')</span><span class="icon-mobile">{{ trans('common.dark_mode') }}</span></button>
     @endif
 </form>
