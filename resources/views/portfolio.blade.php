21161927416b17a7eb570964881f380ec884b5c4
Alexis <alexislegeay@hotmail.fr>
2021-02-12 23:04:09 +0100

    redesign v4

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,7 +1,3 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
-<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
-<link rel="stylesheet" href="{{ versioned_asset('dist/main.css') }}">
-<link rel="stylesheet" href="{{ versioned_asset('dist/bulma-tooltip.css') }}">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 {!! NoCaptcha::renderJs() !!}
 
@@ -42,12 +38,17 @@
 @include('contact')
 
 @include('reseaux')
-<script type="text/javascript" src="{{ URL::asset('js/particles.js') }}"></script>
-<script type="text/javascript" src="{{ URL::asset('js/app_particles.js') }}"></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+<link rel="stylesheet" href="{{ versioned_asset('dist/main.css') }}">
+<link rel="stylesheet" href="{{ versioned_asset('dist/bulma-tooltip.css') }}">
 <script crossorigin="anonymous" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script type="text/javascript" src="{{ URL::asset('/js/jquery.form.js') }}"></script>
 <script src="https://cdn.metroui.org.ua/v4/js/metro.min.js"></script> 
 <script type="text/javascript" src="{{ URL::asset('/js/portfolio.js') }}"></script>
 <script type="text/javascript" src="{{ URL::asset('js/icon.js') }}"></script> 
 <script src="https://cdn.jsdelivr.net/npm/typeit@6.0.3/dist/typeit.min.js"></script>
+<script type="text/javascript" src="{{ URL::asset('js/particles.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('js/app_particles.js') }}"></script>
 @stop
